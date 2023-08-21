788010f8e6b373ff2080bad76001189a9de0faca
Convly <jean-sebastien.herbaux@epitech.eu>
2023-08-21 09:39:19 +0200

    Add project config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const { compilerOptions } = require('./tsconfig.json');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testPathIgnorePatterns: ['lib', 'node_modules'],
+  testMatch: ['**/tests/*.test.ts'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};
